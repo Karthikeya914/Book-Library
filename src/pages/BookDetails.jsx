fb770abbc4e22260c0c18060e4e6f69cf3ce063e
Karthikeya914 <karthikeya.chakravaram@gmail.com>
2025-04-20 19:23:17 +0530

    Create BookDetails.jsx

---
+++ src/pages/BookDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function BookDetails() {
+  return (
+    <div className="page">
+      <h2>Book Details</h2>
+      <p>This page will show more information about the selected book.</p>
+    </div>
+  );
+}
+
+export default BookDetails;
