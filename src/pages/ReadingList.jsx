dc7c3e6e0ac0507e372aa5a31533f6de2fa44294
Karthikeya914 <karthikeya.chakravaram@gmail.com>
2025-04-21 22:33:56 +0530

    Update ReadingList.jsx

--- src/pages/ReadingList.jsx
+++ src/pages/ReadingList.jsx
@@ -1,24 +1,25 @@
 // src/pages/MyList.jsx
 import React from "react";
-import { useBookContext } from "../context/BookContext";
-import BookCard from "../components/BookCard";
+import { useBookContext } from "../context/BookContext"; // Access shared book data from context
+import BookCard from "../components/BookCard"; // Component to display each book info
 import "./MyList.css"; 
 
 const MyList = () => {
-  const { readingList } = useBookContext();
+  const { readingList } = useBookContext(); // Using Context Api to get Books 
 
   return (
     <div className="mylist-container">
       <h1>My Reading List</h1>
       <div className="reading-list-container">
-      {readingList.length > 0 ? (
-        readingList.map((book) => (
-          <BookCard key={book.id} book={book} isRemovable />
-        ))
-      ) : (
-        <p>Your reading list is empty.</p>
-      )}
-    </div>
+        {/* Show books if the reading list is not empty */}
+        {readingList.length > 0 ? (
+          readingList.map((book) => (
+            <BookCard key={book.id} book={book} isRemovable />
+          ))
+        ) : (
+          <p>Your reading list is empty.</p> // Message if no books in the list
+        )}
+      </div>
     </div>
   );
 };
